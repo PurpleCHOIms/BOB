5f26b8a2122099a2ca93513df64a25f1949c99f5
PurpleCHOIms <catower917@gmail.com>
2025-07-14 09:57:14 -0400

    homework714

---
+++ homework714/sum-nbo.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <netinet/in.h>
+
+
+int main(int argc, char* argv[]) {
+    FILE* fp;
+    uint32_t a=0;
+    int sum=0;
+    for(int i=1; i<argc; i++){
+        fp = fopen(argv[i], "rb");
+        if(fp==NULL){
+            return -1;
+        }
+        fread(&a, sizeof(a), 1, fp);
+
+        printf("(%d)%#010x", ntohl(a), ntohl(a));
+        if(i<argc-1){
+            printf(" +");
+        }
+        sum += ntohl(a);
+        fclose(fp);
+    }
+    printf(" = (%d)%#010x", sum, sum);
+    return 0;
+
+}
+
